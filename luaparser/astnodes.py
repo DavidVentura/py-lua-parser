631b1462c8a807f25bef8b6a29ed356df4832820
DavidVentura <davidventura27@gmail.com>
2023-06-24 18:36:04 +0200

    use _set for assign, so _decref/_incref can be called

--- luaparser/astnodes.py
+++ luaparser/astnodes.py
@@ -554,11 +554,11 @@ class Assign(Statement):
             v = self.values[i]
 
             if t.type is Type.TABLE:
-                r.append(f'{t.id} = {v.dump()};')
+                r.append(f'_set(&{t.id}, {v.dump()});')
             elif t.type is Type.UNKNOWN:
-                r.append(f'{t.dump()} = {v.dump()}; // unknown type')
+                r.append(f'_set(&{t.dump()}, {v.dump()}); // unknown type')
             else:
-                r.append(f'{t.dump()} = {v.dump()};')
+                r.append(f'_set(&{t.dump()}, {v.dump()});')
         return '\n'.join(r)
 
 class IAssign(Statement):
